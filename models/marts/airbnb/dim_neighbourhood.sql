c13853fb42809815bfe574e77373f31c11defddb
Luciano Murua <luciano.muruadata@gmail.com>
2026-09-14 20:55:00 +0000

    added incremental materialization to dimensions and fact models

--- models/marts/airbnb/dim_neighbourhood.sql
+++ models/marts/airbnb/dim_neighbourhood.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='neighbourhood_name'
+    )
+}}
+
 with distinct_neighbourhoods as (
 
     select distinct
@@ -5,20 +12,36 @@ with distinct_neighbourhoods as (
         neighbourhood_group
     from {{ ref('stg_airbnb_nyc__listings') }}
 
+    {% if is_incremental() %}
+    -- only look at neighbourhoods that don't have an id assigned yet
+    where neighbourhood not in (select neighbourhood_name from {{ this }})
+    {% endif %}
+
 ),
 
-with_group_id as (
+neighbourhood_groups as (
 
-    select
-        n.neighbourhood,
-        {{ dbt_utils.generate_surrogate_key(['n.neighbourhood_group']) }} as neighbourhood_group_id
-    from distinct_neighbourhoods n
+    select * from {{ ref('dim_neighbourhood_group') }}
+
+),
+
+next_id as (
+
+    {% if is_incremental() %}
+    select coalesce(max(neighbourhood_id), 0) as current_max_id
+    from {{ this }}
+    {% else %}
+    select 0 as current_max_id
+    {% endif %}
 
 )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['neighbourhood']) }} as neighbourhood_id,
-    neighbourhood                                             as neighbourhood_name,
-    neighbourhood_group_id
+    next_id.current_max_id + row_number() over (order by n.neighbourhood) as neighbourhood_id,
+    n.neighbourhood                                                        as neighbourhood_name,
+    ng.neighbourhood_group_id
 
-from with_group_id
+from distinct_neighbourhoods n
+left join neighbourhood_groups ng
+    on n.neighbourhood_group = ng.neighbourhood_group_name
+cross join next_id
--- models/marts/airbnb/dim_room_type.sql
+++ models/marts/airbnb/dim_room_type.sql
@@ -1,12 +1,36 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='room_type_name'
+    )
+}}
+
 with distinct_room_types as (
 
     select distinct room_type
     from {{ ref('stg_airbnb_nyc__listings') }}
 
+    {% if is_incremental() %}
+    -- only look at room types that don't have an id assigned yet
+    where room_type not in (select room_type_name from {{ this }})
+    {% endif %}
+
+),
+
+next_id as (
+
+    {% if is_incremental() %}
+    select coalesce(max(room_type_id), 0) as current_max_id
+    from {{ this }}
+    {% else %}
+    select 0 as current_max_id
+    {% endif %}
+
 )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['room_type']) }} as room_type_id,
-    room_type                                             as room_type_name
+    next_id.current_max_id + row_number() over (order by distinct_room_types.room_type) as room_type_id,
+    distinct_room_types.room_type                                                        as room_type_name
 
 from distinct_room_types
+cross join next_id
